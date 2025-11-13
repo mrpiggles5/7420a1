f5464bb70ac8eccd00177f88ef9cc923eaf99928
yeeeeeeeeeeeeeeeeeeee <65873278+yeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2025-11-13 17:12:51 +1300

    fixing errors

--- reservations/api_urls.py
+++ reservations/api_urls.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 from .api_views import RoomViewSet, ReservationViewSet
 
 router = routers.DefaultRouter()
-router.register(r"rooms", RoomViewSet)
-router.register(r"reservations", ReservationViewSet)
+router.register(r"rooms", RoomViewSet, basename="rooms")
+router.register(r"reservations", ReservationViewSet, basename="reservations")
 
 urlpatterns = router.urls
